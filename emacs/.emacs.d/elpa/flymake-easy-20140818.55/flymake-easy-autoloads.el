32e7e4f5d39cef80e52ae0f629f3094a0ab81f97
marmalmstudent <marmalm@student.chalmers.se>
2017-08-13 15:03:06 +0200

    updated

---
+++ emacs/.emacs.d/elpa/flymake-easy-20140818.55/flymake-easy-autoloads.el
@@ -0,0 +1,16 @@
+;;; flymake-easy-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("flymake-easy.el") (22845 26842 591304
+;;;;;;  946000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; flymake-easy-autoloads.el ends here
