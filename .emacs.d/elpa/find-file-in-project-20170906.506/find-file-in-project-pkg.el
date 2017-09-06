35c8b4d473c3741de75cb5307ad5a0787214c8d6
marmalmstudent <marmalm@student.chalmers.se>
2017-09-07 00:03:32 +0200

    updated emacd and python virtual environment

---
+++ .emacs.d/elpa/find-file-in-project-20170906.506/find-file-in-project-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "find-file-in-project" "20170906.506" "Find file/directory and review Diff/Patch/Commit efficiently everywhere" '((ivy "0.7.0") (emacs "24.3")) :commit "a0e2e8d306400ec88789ff2b3e4b43a91e1600d6" :url "https://github.com/technomancy/find-file-in-project" :keywords '("project" "convenience"))
--- .virtualenvs/venv/bin/activate.fish
+++ .virtualenvs/venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/marcus/.virtualenvs/venv"
+set -gx VIRTUAL_ENV "/home/marcus/git/dotfiles/.virtualenvs/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
